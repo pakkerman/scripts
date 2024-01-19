33068a336b787bf5bb398f703da412feb61d1128
Pakkerman <104409243+Pakkerman@users.noreply.github.com>
2024-01-19 11:44:46 +0800

    feat: add output video with slide number

--- exif-tools/make-slides.sh
+++ exif-tools/make-slides.sh
@@ -1,14 +1,25 @@
 #! /bin/bash
+# This program will take in a path and make slides with the png files inside.
 
-[ ! -d "$1" ] && echo "please input path" && exit 1
+[ ! -d "$1" ] && echo "Invalid input path" && exit 1
+
 
 dir=$(dirname "$1")
 base=$(basename "$1")
-input="$dir/$base/$base-%04d.png"
+input="$dir/$base/$base-%04d.jpg"
 output="$dir/video-$base.mp4"
 
+count=$(find "$1" -type f -name "*.jpg" | grep -c "")
 
-ffmpeg -y -framerate 1.5 -i "$input" -vf "crop=in_w:in_h-30:0:0" -c:v libx264 -preset slow -crf 22 -pix_fmt yuv420p "$output"
+# normal output
+# ffmpeg -y -framerate 2 -i "$input" -c:v libx264 -preset slow -crf 22 -pix_fmt yuv420p "$output"
+
+# output with slide number
+ffmpeg -y -framerate 2 -i "$input" \
+-vf "drawtext=text='%{eif\:trunc(n+1)\:d}\/$count':fontcolor=white:fontsize=48:box=1:boxcolor=black@0.5:boxborderw=5:x=(w-text_w)/2:y=h-th-10" \
+-c:v libx264 -preset slow -crf 22 -pix_fmt yuv420p "$output"
 
 
 
+
+echo "count $count"
