8a8c21a3195bc423f6058ef56774978eeec5638b
Pakkerman <104409243+Pakkerman@users.noreply.github.com>
2024-02-13 12:46:26 +0800

    docs: add comment

--- extension/background.js
+++ extension/background.js
@@ -1,14 +1,10 @@
-console.log('background loaded')
-
+// the default command
 chrome.action.onClicked.addListener((tab) => {
-  console.log(tab)
-  console.log('msg send')
   chrome.tabs.sendMessage(tab.id, { action: 'run' })
 })
 
+// shortcuts and messages are defined in manifest.json
 chrome.commands.onCommand.addListener((command) => {
-  console.log(`call ${command} command`)
-
   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
     chrome.tabs.sendMessage(tabs[0].id, { action: command })
   })
