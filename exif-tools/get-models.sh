c191281c93d5c91e7ed6fa59d52fb2aef39f3837
Pakkerman <104409243+Pakkerman@users.noreply.github.com>
2024-02-20 12:08:36 +0800

    feat: parse adetailer models hashes

--- exif-tools/get-models.sh
+++ exif-tools/get-models.sh
@@ -8,21 +8,13 @@ file="$1"
 
 # Get the directory path of the script
 dir=$(dirname "$0")
-filepath_json="$dir/cache/models.json"
-
-# If no file or file is empty, create and put an empty object into it
-if [ -z "$(jq '.' "$filepath_json")" ]; then
-    mkdir "$dir/cache"
-    echo "{}" > "$filepath_json"
-fi
-
+json_file="$dir/cache/models.json"
 
 # getting metadata from png
 json_string=$("$dir"/get-comment.sh "$file")
 
-
 # get hashes from metadata
-json=$(echo "$json_string" | jq -c '{
+json=$(echo "$json_string" | jq  '{
     "baseModel": {
         "hash": .baseModel.hash
     },
@@ -32,45 +24,26 @@ json=$(echo "$json_string" | jq -c '{
             "weight": .weight
         }
     ]
-}' | jq '.')
+}')
+
+ADtailer=$(echo "$json_string" | jq '{"lora": [.adetailer.args[] | .models[] | {"hash": .hash, "weight": .weight}]}')
+# echo $ADtailer > ~/temp/log
+
 
 # parse hashes into array
 hash_values=($(echo "$json" | jq -r '.baseModel.hash, .lora[].hash'))
+hash_values+=($(echo $ADtailer | jq -r '.lora[].hash'))
 json_array=()
 
-
 for hash_value in "${hash_values[@]}"; do
-    model_info=$(echo $(cat "$filepath_json" | jq --arg key "$hash_value" '.[$key]' | jq '.'))
-    object=$model_info
-    
-    # check if model is already in models.json
-    if [ "$object" == "null" ]; then
-        # fetch data from civitai.com
+    select=$(cat "$json_file" | jq --arg hash "$hash_value" 'select(.hash == $hash)')
+    if [[ -z "$select" ]]; then
+        # new
         response=$(curl -X GET "https://civitai.com/api/v1/model-versions/by-hash/$hash_value" | jq '.')
-        object=$(echo "$response" | jq '{
-            "name": .model.name,
-            "version": .name,
-            "type" : .model.type,
-            "modelVersionId" : .id,
-            "poi": .model.poi
-        }' | jq -a '.')
+        echo "$response" | jq '{name: .model.name, version: .name, hash: .files.[] | .hashes.SHA256, modelVersionId: .id, type: .model.type, poi: .model.poi}' | tee -a "$json_file"
     fi
     
-    echo $(jq --arg key "$hash_value" --argjson value "$object" '. + { ($key): $value }' "$filepath_json") > "$filepath_json"
-    json_array+=("$(echo $(cat "$filepath_json" | jq --arg key "$hash_value" '.[$key]'))")
-    
-    # json_array+=("$(echo $(cat "$filepath_json" | jq --arg key "$hash_value" '{"modelVersionId":.[$key].modelVersionId, "type": .[$key].type }' | jq '.'))")
-    
+    json_array+=($(cat "$json_file" | jq --arg hash "$hash_value" 'select(.hash == $hash)'))
 done
 
 echo "${json_array[@]}" | jq -s -c '.'
-
-
-
-# civitai parsing needs type and id,
-# [{"type":"checkpoint","modelVersionId":132828},
-# {"type":"embed","weight":1,"modelVersionId":77169},
-# {"type":"lora","weight":-1,"modelVersionId":121575},
-# {"type":"lora","weight":0.4,"modelVersionId":71871},
-# {"type":"lora","weight":0.2,"modelVersionId":100859},
-# {"type":"lora","weight":-0.4,"modelVersionId":126824}]
