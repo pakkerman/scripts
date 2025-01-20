9ff8a76a5eb17c838df2182b8263978851475f51
Pakkerman <104409243+Pakkerman@users.noreply.github.com>
2025-01-20 15:47:19 +0800

    chore: housekeeping

--- exiftools/make-slides.sh
+++ exiftools/make-slides.sh
@@ -12,13 +12,13 @@ count=$(find "$1" -type f -name "*.jpg" | grep -c "")
 
 # normal output
 ffmpeg \
-	-loglevel warning \
-	-y \
-	-framerate 1.2 \
-	-i "$input" -c:v libx264 \
-	-preset slow -crf 16 \
-	-pix_fmt yuv420p \
-	-vf "pad=ceil(iw/2)*2:ceil(ih/2)*2" \
-	"$output"
+  -loglevel warning \
+  -y \
+  -framerate 1.2 \
+  -i "$input" -c:v libx264 \
+  -preset slow -crf 16 \
+  -pix_fmt yuv420p \
+  -vf "pad=ceil(iw/2)*2:ceil(ih/2)*2" \
+  "$output"
 
 echo "count $count"
