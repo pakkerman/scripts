ac302e5f86fe871ef1aff932ceba33f23f1d95b1
Pakkerman <104409243+Pakkerman@users.noreply.github.com>
2026-04-16 16:01:54 +0800

    feat: random watermark position

--- ComfyUI_tools/lib/encode_video.sh
+++ ComfyUI_tools/lib/encode_video.sh
@@ -1,5 +1,27 @@
 #!/usr/bin/env bash
 
+x_pad=20
+y_pad=40
+corner=$((RANDOM % 3))
+
+case "$corner" in
+0)
+  overlay_x="W-w-$x_pad"
+  overlay_y="$y_pad"
+  ;;
+1)
+  overlay_x="$x_pad"
+  overlay_y="H-h-$y_pad"
+  ;;
+2)
+  overlay_x="W-w-$x_pad"
+  overlay_y="H-h-$y_pad"
+  ;;
+esac
+
+grain=18
+gaussian_blur=0.4
+
 ffmpeg \
   -y \
   -i "$1" \
@@ -12,16 +34,16 @@ ffmpeg \
   hqdn3d=3:3:6:6,
   unsharp=luma_msize_x=3:luma_msize_y=3:luma_amount=1:chroma_msize_x=3:chroma_msize_y=3:chroma_amount=1,
   vignette=angle=PI/8,
-  noise=c0s=20:c0f=t+u,
-  gblur=sigma=0.4,
+  noise=c0s=$grain:c0f=t+u,
+  gblur=sigma=$gaussian_blur,
   rgbashift=rh=-0.4:gh=0.4[v0];
-  [v0][1:v]overlay=W-w-30:H-h-75,
+  [v0][1:v]overlay=$overlay_x:$overlay_y,
   format=yuv420p[v]
   " \
   -map "[v]" \
   -c:v libx265 \
   -crf 26 \
-  -preset medium \
+  -preset slow \
   -tune grain \
   -pix_fmt yuv420p \
   -tag:v hvc1 \
@@ -29,37 +51,3 @@ ffmpeg \
   -shortest \
   -movflags +faststart \
   "${1%.*}"_output.mp4
-
-# -r 24 -g 48 -keyint_min 48 \#!/usr/bin/env bash
-
-# ffmpeg \
-#   -y \
-#   -i "$1" \
-#   -i "$HOME/Documents/assets/watermark.png" \
-#   -f lavfi -i anullsrc=cl=stereo:r=48000 \
-#   -loglevel info \
-#   -filter_complex "
-#   [0:v]scale=1080:1920:force_original_aspect_ratio=decrease:flags=spline,
-#   hqdn3d=3:3:6:6,
-#   unsharp=luma_msize_x=3:luma_msize_y=3:luma_amount=1:chroma_msize_x=3:chroma_msize_y=3:chroma_amount=1,
-#   vignette=angle=PI/8,
-#   noise=c0s=24:c0f=t+u,
-#   gblur=sigma=0.6,
-#   rgbashift=rh=-0.6:gh=0.6[v0];
-#   [v0][1:v]overlay=W-w-30:H-h-75,format=yuv420p[v]
-#   " \
-#   -map "[v]" \
-#   -map 2:a \
-#   -c:v libx265 \
-#   -crf 22 \
-#   -preset medium \
-#   -tune grain \
-#   -pix_fmt yuv420p \
-#   -tag:v hvc1 \
-#   -level 4.1 \
-#   -c:a aac -b:a 160k -ar 48000 -ac 2 \
-#   -shortest \
-#   -movflags +faststart \
-#   "${1%.*}"_output.mp4
-#
-# # -r 24 -g 48 -keyint_min 48 \
